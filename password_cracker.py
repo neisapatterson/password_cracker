13400914766c0692f66e58dd1a569791484806a9
neisapatterson <97197329+neisapatterson@users.noreply.github.com>
2023-11-11 18:44:23 -0500

    Eliminated duplicates in customer character sets.

--- password_cracker.py
+++ password_cracker.py
@@ -4,9 +4,9 @@ from passlib.hash import sha512_crypt, md5_crypt
 LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
 UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 INT = "0123456789"
-ASCII = "".join(chr(i) for i in range(33, 126 + 1))
+ASCII = "".join(chr(i) for i in range(33, 126 + 1)) 
 
-def main(file, min_val, max_val, charset):
+def main(file, min_val, max_val, charset, output):
     # hashed passwords will be stored as the key with format, salt, and the remaining string as the input
     passwords = {}
 
@@ -15,12 +15,21 @@ def main(file, min_val, max_val, charset):
             line = line.strip()
             passwords[line] = [line.split('$')[1], line.split('$')[2], line.split('$')[3]]
 
+    if output != "":
+        output_file = open(output, 'w') 
+    else: 
+        output_file = None
+    
+
     for password in passwords:
-        crack(password, passwords[password][0], passwords[password][1], passwords[password][2], min_val, max_val, charset, passwords_length)
+        crack(password, passwords[password][0], passwords[password][1], passwords[password][2], min_val, max_val, charset, len(passwords), output_file)
 
-def crack(password, format, salt, orginal_string, min_val, max_val, charset, passwords_length):
+    if output_file != None: 
+        output_file.close()
+
+def crack(password, format, salt, orginal_string, min_val, max_val, charset, passwords_length, output_file):
     cracked_passwords = []
-
+    
     match charset:
         case "lowercase":
             charset=LOWERCASE
@@ -31,16 +40,19 @@ def crack(password, format, salt, orginal_string, min_val, max_val, charset, pas
         case "ascii":
             charset=ASCII
         case _:
-            charset=charset
+            charset="".join(set(charset))
 
     for guess in generate_combinations("", charset, min_val, max_val):
-        if check_password(orginal_string, salt, guess, format):
+        if check_password(password, salt, guess, format):
             print(f"{password} decrypts to {guess}")
-            cracked_passwords.append(guess)
-            if len(cracked) == passwords_length:
-                exit(0)
 
-            
+            cracked_passwords.append(guess)
+            if output_file != None:
+                output_file.write(password + ":" + guess)
+            if len(cracked_passwords) == passwords_length:
+                if output_file != None:
+                    output_file.close()
+                exit(0)
 
 
 def generate_combinations(curr_string, possible, min_val, max_val):
@@ -48,18 +60,17 @@ def generate_combinations(curr_string, possible, min_val, max_val):
         for i in range(len(possible)):
             new_combination = curr_string + possible[i]
             if len(new_combination) >= min_val and len(new_combination) <= max_val:
-                print(new_combination)
                 yield new_combination
 
-            yield from generate_combinations(new_combination, possible[:i] + possible[i + 1:])
+            yield from generate_combinations(new_combination, possible[:i] + possible[i + 1:], min_val, max_val)
 
 def check_password(enc_pass, salt, guess, format):
-    if format == 6:
+    if format == "6":
         hashed_guess = sha512_crypt.using(salt=salt, rounds=5000).hash(guess)
-        return sha512_crypt.verify(hashed_guess, enc_pass)
-    elif format == 1:
-        hashed_guess = md5_crypt.using(salt=salt, rounds=5000).hash(guess)
-        return md5_crypt.verify(hashed_guess, enc_pass)   
+        return hashed_guess == enc_pass
+    elif format == "1":
+        hashed_guess = md5_crypt.using(salt=salt).hash(guess)
+        return hashed_guess == enc_pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Password Cracker", description="This password cracker will generate all possible combinations of a given character set and length and attempt to decrypt given hashed passwords.")
@@ -72,6 +83,8 @@ if __name__ == "__main__":
 
     parser.add_argument("CHARSET", type=str, help="Types of characters to use. Choose from: 'lowercase', 'uppercase', 'int', 'ascii' or enter your own charset.", metavar="charset")
 
+    parser.add_argument("--OUTPUT", type=str, default="", help="Save cracked passwords to a file.", metavar="output")
+
     args = parser.parse_args()
 
-    main(file=args.FILE, min_val=args.MIN_VAL, max_val=args.MAX_VAL, charset=args.CHARSET)
+    main(file=args.FILE, min_val=args.MIN_VAL, max_val=args.MAX_VAL, charset=args.CHARSET, output=args.OUTPUT)
